b6850b5980f2d67b8c661ef102fd557e630c5e56
Chris Wiederspan <chris@wiederspan.com>
2020-07-23 15:29:23 +0000

    Updated the build script

---
+++ src/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aks.Services.Web.Models {
+
+    public class Data {
+
+        public Data(string self, Data child = null) {
+            this.Self = self;
+            this.Child = child;
+        }
+
+        public string Self { get; set; }
+
+        public Data Child { get; set; }
+    }
+}
